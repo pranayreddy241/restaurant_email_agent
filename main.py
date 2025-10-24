e09b11b321370b688e0dda465cef47d46980b112
pranayreddy241 <81856587+pranayreddy241@users.noreply.github.com>
2025-10-24 17:00:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ from datetime import datetime
 #   export SMTP_SERVER="smtp.example.com"
 #   export SMTP_PORT=465
 
-EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS", "your_email@example.com")
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD", "your_password")
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS", "20131a0522@gvpce.ac.in")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD", "pranayreddy522")
 IMAP_SERVER = os.environ.get("IMAP_SERVER", "imap.gmail.com")
 SMTP_SERVER = os.environ.get("SMTP_SERVER", "smtp.gmail.com")
 SMTP_PORT = int(os.environ.get("SMTP_PORT", "587"))
